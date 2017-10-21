d7f2bd2b4ad742b40753cffda0c46beb71ce7887
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-10-21 20:57:00 +0300

    move respond method body to Response utility class

--- src/Response.php
+++ src/Response.php
@@ -4,10 +4,61 @@ namespace CarbonFramework;
 
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Psr7\Response as Psr7Response;
+use Psr\Http\Message\ResponseInterface;
 
 class Response {
 	public static function response() {
 		return new Psr7Response();
+	}
+
+	/**
+	 * @credit slimphp/slim Slim/App.php
+	 */
+	public static function respond( ResponseInterface $response ) {
+		// Send response
+        if (!headers_sent()) {
+            // Status
+            header(sprintf(
+                'HTTP/%s %s %s',
+                $response->getProtocolVersion(),
+                $response->getStatusCode(),
+                $response->getReasonPhrase()
+            ));
+            // Headers
+            foreach ($response->getHeaders() as $name => $values) {
+                foreach ($values as $value) {
+                    header(sprintf('%s: %s', $name, $value), false);
+                }
+            }
+        }
+        // Body
+        $body = $response->getBody();
+        if ($body->isSeekable()) {
+            $body->rewind();
+        }
+        $chunkSize = 4096;
+        $contentLength = $response->getHeaderLine('Content-Length');
+        if (!$contentLength) {
+            $contentLength = $body->getSize();
+        }
+        if (isset($contentLength)) {
+            $amountToRead = $contentLength;
+            while ($amountToRead > 0 && !$body->eof()) {
+                $data = $body->read(min($chunkSize, $amountToRead));
+                echo $data;
+                $amountToRead -= strlen($data);
+                if (connection_status() != CONNECTION_NORMAL) {
+                    break;
+                }
+            }
+        } else {
+            while (!$body->eof()) {
+                echo $body->read($chunkSize);
+                if (connection_status() != CONNECTION_NORMAL) {
+                    break;
+                }
+            }
+        }
 	}
 
 	public static function output( $response, $output ) {
