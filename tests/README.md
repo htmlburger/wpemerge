0afa18723b06c803371d268a298e0e49b8b8c6a1
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-10-22 17:40:10 +0300

    setup WordPress test environment

---
+++ tests/README.md
@@ -0,0 +1,21 @@
+# Carbon Framework Unit Tests
+
+## Initial Setup
+
+Install WordPress and the WP Unit Test lib using the `install.sh` script. Change to the framework root directory and type:
+
+    $ tests/bin/install.sh <db-name> <db-user> <db-password> [db-host] [wp-version]
+
+Sample usage:
+
+    $ tests/bin/install.sh carbon_framework_tests root root localhost 4.8
+
+**Important**: Make sure that the `<db-name>` database has been created. Note that all data will be removed during testing.
+
+## Running Tests
+
+Simply change to the framework root directory and type:
+
+    $ ./vendor/bin/phpunit
+
+Refer to the [phpunit command line test runner reference](https://phpunit.de/manual/current/en/phpunit-book.html#textui) for more information and command line options.
--- tests/unit-tests/UrlTest.php
+++ tests/unit-tests/UrlTest.php
@@ -8,24 +8,20 @@ class UrlTest extends TestCase {
     public function testAddLeadingSlash() {
         $this->assertEquals( '/example', Url::addLeadingSlash('example') );
         $this->assertEquals( '/example', Url::addLeadingSlash('/example') );
-        $this->assertEquals( '//example', Url::addLeadingSlash('//example') );
     }
 
     public function testRemoveLeadingSlash() {
         $this->assertEquals( 'example', Url::removeLeadingSlash('/example') );
-        $this->assertEquals( 'example', Url::removeLeadingSlash('//example') );
         $this->assertEquals( 'example', Url::removeLeadingSlash('example') );
     }
 
     public function testAddTrailingSlash() {
         $this->assertEquals( 'example/', Url::addTrailingSlash('example') );
         $this->assertEquals( 'example/', Url::addTrailingSlash('example/') );
-        $this->assertEquals( 'example//', Url::addTrailingSlash('example//') );
     }
 
     public function testRemoveTrailingSlash() {
         $this->assertEquals( 'example', Url::removeTrailingSlash('example/') );
-        $this->assertEquals( 'example', Url::removeTrailingSlash('example//') );
         $this->assertEquals( 'example', Url::removeTrailingSlash('example') );
     }
 }
