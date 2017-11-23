cc5a21fc182fbe400741f9fbce19654232c0f192
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-11-23 15:01:45 +0200

    move list of framework condition types to a static property

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -12,6 +12,23 @@ use CarbonFramework\ServiceProviders\ServiceProviderInterface;
  */
 class RoutingServiceProvider implements ServiceProviderInterface {
 	/**
+	 * Key=>Class dictionary of condition types
+	 *
+	 * @var string[string]
+	 */
+	protected static $condition_class_dictionary = [
+		'url' => \CarbonFramework\Routing\Conditions\Url::class,
+		'custom' => \CarbonFramework\Routing\Conditions\Custom::class,
+		'multiple' => \CarbonFramework\Routing\Conditions\Multiple::class,
+		'post_id' => \CarbonFramework\Routing\Conditions\PostId::class,
+		'post_slug' => \CarbonFramework\Routing\Conditions\PostSlug::class,
+		'post_template' => \CarbonFramework\Routing\Conditions\PostTemplate::class,
+		'post_type' => \CarbonFramework\Routing\Conditions\PostType::class,
+		'query_var' => \CarbonFramework\Routing\Conditions\QueryVar::class,
+		'has_query_var' => \CarbonFramework\Routing\Conditions\HasQueryVar::class,
+	];
+
+	/**
 	 * {@inheritDoc}
 	 */
 	public function register( $container ) {
@@ -25,15 +42,9 @@ class RoutingServiceProvider implements ServiceProviderInterface {
 			$container['framework.routing.global_middleware']
 		);
 
-		$container['framework.routing.conditions.custom'] = \CarbonFramework\Routing\Conditions\Custom::class;
-		$container['framework.routing.conditions.url'] = \CarbonFramework\Routing\Conditions\Url::class;
-		$container['framework.routing.conditions.multiple'] = \CarbonFramework\Routing\Conditions\Multiple::class;
-		$container['framework.routing.conditions.post_id'] = \CarbonFramework\Routing\Conditions\PostId::class;
-		$container['framework.routing.conditions.post_slug'] = \CarbonFramework\Routing\Conditions\PostSlug::class;
-		$container['framework.routing.conditions.post_template'] = \CarbonFramework\Routing\Conditions\PostTemplate::class;
-		$container['framework.routing.conditions.post_type'] = \CarbonFramework\Routing\Conditions\PostType::class;
-		$container['framework.routing.conditions.query_var'] = \CarbonFramework\Routing\Conditions\QueryVar::class;
-		$container['framework.routing.conditions.has_query_var'] = \CarbonFramework\Routing\Conditions\HasQueryVar::class;
+		foreach ( static::$condition_class_dictionary as $key => $class ) {
+			$container[ 'framework.routing.conditions.' . $key ] = $class;
+		}
 
 		$container['framework.routing.router'] = function() {
 			return new \CarbonFramework\Routing\Router();
