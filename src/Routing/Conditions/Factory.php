a585d7bd44d2645b05ed68007ff79d654ad9e159
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-11-23 12:26:54 +0200

    allow conditions specified with an array to omit the "custom" string as the first parameter

--- src/Routing/Conditions/Factory.php
+++ src/Routing/Conditions/Factory.php
@@ -17,7 +17,7 @@ class Factory {
 	/**
 	 * Create a new condition
 	 *
-	 * @param  string|Condition|array $options
+	 * @param  string|array|Closure $options
 	 * @return ConditionInterface
 	 */
 	public static function make( $options ) {
@@ -25,15 +25,34 @@ class Factory {
 			return static::makeFromUrl( $options );
 		}
 
-		if ( is_a( $options, Closure::class ) ) {
-			return static::makeFromClosure( $options );
-		}
-
 		if ( is_array( $options ) ) {
 			return static::makeFromArray( $options );
 		}
 
+		if ( is_a( $options, Closure::class ) ) {
+			return static::makeFromClosure( $options );
+		}
+
 		throw new InvalidRouteConditionException( 'Invalid condition options supplied.' );
+	}
+
+	/**
+	 * Check if the passed argument is a registered condition type
+	 *
+	 * @param  mixed   $condition_type
+	 * @return boolean
+	 */
+	protected static function conditionTypeRegistered( $condition_type ) {
+		if ( ! is_string( $condition_type ) ) {
+			return false;
+		}
+
+		$condition_class = Framework::resolve( 'framework.routing.conditions.' . $condition_type );
+		if ( $condition_class === null ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -57,6 +76,16 @@ class Factory {
 	}
 
 	/**
+	 * Create a new condition from an array of conditions
+	 *
+	 * @param  array               $options
+	 * @return ConditionInterface
+	 */
+	protected static function makeFromArrayOfConditions( $options ) {
+		return new MultipleCondition( $options );
+	}
+
+	/**
 	 * Create a new condition from an array
 	 *
 	 * @param  array               $options
@@ -74,23 +103,19 @@ class Factory {
 		$condition_type = $options[0];
 		$arguments = array_slice( $options, 1 );
 
-		$condition_class = Framework::resolve( 'framework.routing.conditions.' . $condition_type );
-		if ( $condition_class === null ) {
-			throw new Exception( 'Unknown condition type specified: ' . $condition_type );
+		if ( ! $this->conditionTypeRegistered( $condition_type ) ) {
+			if ( is_callable( $condition_type ) ) {
+				$condition_type = 'custom';
+				$arguments = $options;
+			} else {
+				throw new Exception( 'Unknown condition type specified: ' . $condition_type );
+			}
 		}
+
+		$condition_class = Framework::resolve( 'framework.routing.conditions.' . $condition_type );
 
 		$reflection = new ReflectionClass( $condition_class );
 		$condition = $reflection->newInstanceArgs( $arguments );
 		return $condition;
-	}
-
-	/**
-	 * Create a new condition from an array of conditions
-	 *
-	 * @param  array               $options
-	 * @return ConditionInterface
-	 */
-	protected static function makeFromArrayOfConditions( $options ) {
-		return new MultipleCondition( $options );
 	}
 }
