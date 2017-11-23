de79368d8d10946b4cf0c250ac44c2978e20003b
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-11-23 18:04:32 +0200

    fix template.php code coverage ignore declaration

--- src/template.php
+++ src/template.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Template used to override the loaded template file by WordPress when a route is handled
- *
- * @codeCoverageIgnore
  */
+// @codeCoverageIgnoreStart
 use Obsidian\Framework;
 $response = apply_filters( 'obsidian_response', null );
 if ( $response !== null ) {
 	Framework::respond( $response );
 }
+// @codeCoverageIgnoreEnd
