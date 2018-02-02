aa567cd0ee066bc3c89dec5a240bc687193ef372
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2018-02-03 00:41:12 +0200

    add HasContextTrait tests

---
+++ tests/unit-tests/View/HasContextTraitTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WPEmergeTests\View;
+
+use Mockery;
+use WPEmerge\View\HasContextTrait;
+use WP_UnitTestCase;
+
+/**
+ * @coversDefaultClass \WPEmerge\View\HasContextTrait
+ */
+class HasContextTraitTest extends WP_UnitTestCase {
+	/**
+	 * @covers ::getContext
+	 * @covers ::with
+	 */
+	public function testGetContext() {
+		$subject = $this->getMockForTrait( HasContextTrait::class );
+
+		$subject->with( 'foo', 'foobar' );
+		$subject->with( [
+			'bar' => 'barbar',
+			'baz' => 'bazbar',
+		] );
+		$this->assertSame( [
+			'foo' => 'foobar',
+			'bar' => 'barbar',
+			'baz' => 'bazbar',
+		], $subject->getContext() );
+	}
+}
