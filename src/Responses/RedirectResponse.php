3d9c09c37523ce7be6898dc0dad3243c13daf7b8
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2018-02-03 20:56:20 +0200

    avoid class ambiguity

--- src/Responses/RedirectResponse.php
+++ src/Responses/RedirectResponse.php
@@ -2,15 +2,13 @@
 
 namespace WPEmerge\Responses;
 
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Psr7\Response;
-use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Psr7\Response as Psr7Response;
 use WPEmerge\Requests\Request;
 
 /**
  * A collection of tools for the creation of responses
  */
-class RedirectResponse extends Response {
+class RedirectResponse extends Psr7Response {
 	/**
 	 * Current request.
 	 *
@@ -31,9 +29,9 @@ class RedirectResponse extends Response {
 	/**
 	 * Get a response redirecting to a specific url.
 	 *
-	 * @param  string            $url
-	 * @param  integer           $status
-	 * @return ResponseInterface
+	 * @param  string                              $url
+	 * @param  integer                             $status
+	 * @return \Psr\Http\Message\ResponseInterface
 	 */
 	public function to( $url, $status = 302 ) {
 		return $this->withHeader( 'Location', $url )->withStatus( $status );
@@ -42,9 +40,9 @@ class RedirectResponse extends Response {
 	/**
 	 * Get a response redirecting back to the referrer or a fallback.
 	 *
-	 * @param  string            $fallback
-	 * @param  integer           $status
-	 * @return ResponseInterface
+	 * @param  string                              $fallback
+	 * @param  integer                             $status
+	 * @return \Psr\Http\Message\ResponseInterface
 	 */
 	public function back( $fallback = '', $status = 302 ) {
 		$url = $this->request->headers( 'Referer' );
