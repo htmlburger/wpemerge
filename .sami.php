13f62b0f1ef6840089e001f15d13529e08526c3d
Atanas Angelov <atanas.angelov.dev@gmail.com>
2018-10-23 00:00:48 +0300

    fix inconsistent indentation in .sami.php

--- .sami.php
+++ .sami.php
@@ -11,14 +11,14 @@ $dir = __DIR__ . DS . $relative_dir;
 $documentation_dir = __DIR__ . DS . 'api-reference';
 
 $versions = GitVersionCollection::create($dir)
-    ->addFromTags('*.*.*')
-    ->add('master', 'master branch');
+	->addFromTags('*.*.*')
+	->add('master', 'master branch');
 
 return new \Sami\Sami( $dir, [
 	'title' => 'WP Emerge',
 	'versions' => $versions,
 	'remote_repository' => new GitHubRemoteRepository( $repository, dirname( $dir ) ),
-    'build_dir' => $documentation_dir . DS . 'build' . DS . '%version%',
-    'cache_dir' => $documentation_dir . DS . 'cache' . DS . '%version%',
-    'filter' => new PublicFilter(),
+	'build_dir' => $documentation_dir . DS . 'build' . DS . '%version%',
+	'cache_dir' => $documentation_dir . DS . 'cache' . DS . '%version%',
+	'filter' => new PublicFilter(),
 ] );
