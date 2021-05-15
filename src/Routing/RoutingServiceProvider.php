aeb6585d67ea9af5a55bcc7dc88e3cc17974a80f
Atanas Angelov <hi@atanas.dev>
2021-05-16 00:12:07 +0300

    Add App::router() to access the router instance for convenience

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -92,6 +92,7 @@ class RoutingServiceProvider implements ServiceProviderInterface {
 		} );
 
 		$app = $container[ WPEMERGE_APPLICATION_KEY ];
+		$app->alias( 'router', WPEMERGE_ROUTING_ROUTER_KEY );
 		$app->alias( 'route', WPEMERGE_ROUTING_ROUTE_BLUEPRINT_KEY );
 		$app->alias( 'routeUrl', WPEMERGE_ROUTING_ROUTER_KEY, 'getRouteUrl' );
 	}
